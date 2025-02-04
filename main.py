131493d8c4498386ee0ed1d922a34fa026fdf394
nmquang003 <sinper003@gmail.com>
2025-02-04 09:02:02 +0000

    from colab

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
-from methods.ct_loss import CT_loss, sim
+from methods.ct_loss import constractive_loss, sim
 import torch
 
 a = torch.tensor([[1, 2, 3], [4, 5, 6], [7, 8, 9]], dtype=torch.float32)
 
-b = torch.tensor([[1, 2, 3], [4, 5, 4], [7, 8, 9]], dtype=torch.float32)
+b = torch.tensor([[[1, 2, 3], [1, 2, 1]], [[4, 5, 0], [4, 5, 3]], [[7, 8, 9], [1, 2, 3]]], dtype=torch.float32)
 
-c = torch.tensor([[1, 2, 3], [4, 5, 6], [7, 8, 9]], dtype=torch.float32)
+c = torch.tensor([[[1, 2, 3]], [[4, 5, 6]], [[7, 8, 9]]], dtype=torch.float32)
 
 print(sim(a, b))
 
-print(CT_loss(a, b, c))
-
+print(constractive_loss(a, b, c))
