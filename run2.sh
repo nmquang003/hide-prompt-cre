e5ddfff0b38257ab9a6a59bf503bf2f94e6095c5
nmquang003 <sinper003@gmail.com>
2025-02-18 11:09:06 +0700

    all setting

--- run2.sh
+++ run2.sh
@@ -4,9 +4,13 @@
 # Each combination is a string with the following format:
 # "dataname seed encoder_epochs prompt_pool_epochs classifier_epochs prompt_length prompt_top_k prompt_pool_size beta"
 combinations=(
-    "FewRel 2021 15 10 200 8 4 8 0.1" # Best setting
-    "FewRel 2021 15 10 200 8 4 8 0" # w/o description
-    "FewRel 2021 15 10 200 8 1 1 0" # w/o description and prompt pool
+    "FewRel 2021 15 10 200 8 4 8 0.1 1" # Best setting, lk=84
+    "FewRel 2021 15 10 200 8 4 8 0 0" # w/o description
+    "FewRel 2021 15 10 200 8 1 1 0 0" # w/o description and prompt pool
+    "FewRel 2021 15 10 200 8 1 8 0.1 1" # Best setting, lk=81
+    "FewRel 2021 15 10 200 8 2 8 0.1 1" # Best setting, lk=82
+    "FewRel 2021 15 10 200 2 4 8 0.1 1" # Best setting, lk=24
+    "FewRel 2021 15 10 200 4 4 8 0.1 1" # Best setting, lk=42
 )
 
 # Loop over each combination
@@ -22,6 +26,7 @@ for combination in "${combinations[@]}"; do
     prompt_top_k=$7
     prompt_pool_size=$8
     beta=$9
+    num_descriptions=${10}
 
     echo "Running with 
     --dataname=$dataname,
@@ -55,6 +60,6 @@ for combination in "${combinations[@]}"; do
         --replay_epochs 200 \
         --classifier_lr 5e-5 \
         --prompt_type only_prompt \
-        --num_descriptions 1 \
+        --num_descriptions $num_descriptions \
         --beta $beta
 done
